66800c050fa88c0ce05e53939cb3dafea8ffec8e
Tudor Pavel <tudor@chartmogul.com>
2023-12-01 11:00:21 +0200

    Solve day01 part 2

--- day01/day01.rb
+++ day01/day01.rb
@@ -2,12 +2,42 @@
 
 class Day01
   def solve(lines)
+    p1 = part1(lines)
+    p2 = part2(lines)
+
+    [p1, p2]
+  end
+
+  private
+
+  DIGITS = %w[zero one two three four five six seven eight nine].freeze
+  # Spelled out digits can overlap for example 'oneight' -> 18
+  # This regex uses a positive lookahead to find overlapping matches
+  # Sauce: https://stackoverflow.com/a/41028635/2707376
+  REGEX2 = /(?=(#{DIGITS.join('|')}|\d))/
+
+  def part1(lines)
     nums = lines.map do |str|
       digits = str.chars.filter { |c| c =~ /[0-9]/ }
       [digits.first, digits.last].join.to_i
     end
-    p1 = nums.reduce(&:+)
 
-    [p1, 2]
+    nums.sum
+  end
+
+  def part2(lines)
+    nums = lines.map do |str|
+      digits = str.scan(REGEX2).flatten
+      [
+        to_digit(digits.first),
+        to_digit(digits.last)
+      ].join.to_i
+    end
+
+    nums.sum
+  end
+
+  def to_digit(str)
+    (DIGITS.index(str) || str).to_s
   end
 end
--- spec/day01/day01_spec.rb
+++ spec/day01/day01_spec.rb
@@ -5,21 +5,43 @@ require_relative '../../day01/day01'
 RSpec.describe Day01 do
   subject(:solve) { described_class.new.solve(lines) }
 
-  let(:lines) do
-    %w[
-      1abc2
-      pqr3stu8vwx
-      a1b2c3d4e5f
-      treb7uchet
-    ]
-  end
-
   describe '#solve' do
-    it 'solves both parts', :aggregate_failures do
-      p1, p2 = solve
+    context 'when part 1' do
+      let(:lines) do
+        %w[
+          1abc2
+          pqr3stu8vwx
+          a1b2c3d4e5f
+          treb7uchet
+        ]
+      end
 
-      expect(p1).to eq(142)
-      expect(p2).to eq(2)
+      it 'returns the right result' do
+        p1, _p2 = solve
+
+        expect(p1).to eq(142)
+      end
+    end
+
+    context 'when part 2' do
+      let(:lines) do
+        %w[
+          two1nine
+          eightwothree
+          abcone2threexyz
+          xtwone3four
+          4nineeightseven2
+          zoneight234
+          7pqrstsixteen
+          oneight
+        ]
+      end
+
+      it 'returns the right result' do
+        _p1, p2 = solve
+
+        expect(p2).to eq(299)
+      end
     end
   end
 end
