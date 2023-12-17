8c24683dd9ccd8fbb1d0c1b4f5fa5462ae2d0697
Tudor Pavel <tudor@chartmogul.com>
2023-12-17 07:42:17 +0200

    Solve day08 part 2

--- day08/day08.rb
+++ day08/day08.rb
@@ -5,17 +5,38 @@ class Day08
     @dirs = lines[0]
     init_nodes(lines)
 
-    [part1, -2]
+    [part1, part2]
   end
 
   private
 
   def part1
-    node = @start
+    @start_nodes.each do |node|
+      return step_count(node) if node.id == 'AAA'
+    end
+  end
+
+  # Checking the input it seems each start node only ever
+  # sees exactly one end node so each path likely ends in
+  # a loop at some point with no intersections between them.
+  #
+  # Idea: find the length of each loop and find the least
+  # common multiple (LCM) between the lengths.
+  #
+  # Caveat: the number of steps to reach the start of each
+  # loop could be different but after checking the input
+  # it seems Eric was nice enough to make all loops start
+  # at the start node.
+  def part2
+    @start_nodes.map { |n| step_count(n) }.reduce(:lcm)
+  end
+
+  def step_count(start_node)
+    node = start_node
     dir = 0
     step_count = 0
 
-    while node != @end
+    until node.id.end_with?('Z')
       node = @dirs[dir] == 'L' ? node.left : node.right
       dir = (dir + 1) % @dirs.size
       step_count += 1
@@ -26,6 +47,7 @@ class Day08
 
   def init_nodes(lines) # rubocop:disable Metrics
     lookup = {}
+    @start_nodes = []
 
     tuples = lines[2..].map do |l|
       l.scan(/(\w+) = \((\w+), (\w+)\)/).flatten
@@ -33,8 +55,7 @@ class Day08
 
     tuples.each do |(id)|
       lookup[id] = Node.new(id)
-      @start = lookup[id] if id == 'AAA'
-      @end = lookup[id] if id == 'ZZZ'
+      @start_nodes << lookup[id] if id.end_with?('A')
     end
 
     tuples.each do |(id, left, right)| # rubocop:disable Style/CombinableLoops
@@ -44,6 +65,7 @@ class Day08
   end
 
   class Node
+    attr_reader :id
     attr_accessor :left, :right
 
     def initialize(id)
--- spec/day08/day08_spec.rb
+++ spec/day08/day08_spec.rb
@@ -5,34 +5,26 @@ require_relative '../../day08/day08'
 RSpec.describe Day08 do
   subject(:solve) { described_class.new.solve(lines) }
 
-  let(:lines) do
-    [
-      'RL',
-      '',
-      'AAA = (BBB, CCC)',
-      'BBB = (DDD, EEE)',
-      'CCC = (ZZZ, GGG)',
-      'DDD = (DDD, DDD)',
-      'EEE = (EEE, EEE)',
-      'GGG = (GGG, GGG)',
-      'ZZZ = (ZZZ, ZZZ)'
-    ]
-  end
-
   describe '#solve' do
     context 'when part 1' do
+      let(:lines) do
+        [
+          'RL',
+          '',
+          'AAA = (BBB, CCC)',
+          'BBB = (DDD, EEE)',
+          'CCC = (ZZZ, GGG)',
+          'DDD = (DDD, DDD)',
+          'EEE = (EEE, EEE)',
+          'GGG = (GGG, GGG)',
+          'ZZZ = (ZZZ, ZZZ)'
+        ]
+      end
+
       it 'returns the right result' do
         p1, _p2 = solve
 
         expect(p1).to eq(2)
-      end
-    end
-
-    context 'when part 2' do
-      it 'returns the right result' do
-        _p1, p2 = solve
-
-        expect(p2).to eq(-2)
       end
     end
 
@@ -51,6 +43,29 @@ RSpec.describe Day08 do
         p1, _p2 = solve
 
         expect(p1).to eq(6)
+      end
+    end
+
+    context 'when part 2' do
+      let(:lines) do
+        [
+          'LR',
+          '',
+          '11A = (11B, XXX)',
+          '11B = (XXX, 11Z)',
+          '11Z = (11B, XXX)',
+          '22A = (22B, XXX)',
+          '22B = (22C, 22C)',
+          '22C = (22Z, 22Z)',
+          '22Z = (22B, 22B)',
+          'XXX = (XXX, XXX)'
+        ]
+      end
+
+      it 'returns the right result' do
+        _p1, p2 = solve
+
+        expect(p2).to eq(6)
       end
     end
   end
